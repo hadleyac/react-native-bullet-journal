73d3c855e6323666b120764913e6eec5b898d260
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-09-22 17:58:01 -0500

    Allow user to add new tasks
    Added a basic input text box and button, which allows the user to type in text, and add it to a list of tasks in state. React-Native-Paper is being used for the visual components.

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet,
+  SafeAreaView, 
+  Text, 
+  // TextInput, 
+  View, 
+  // Button 
+} from 'react-native';
+import Constants from 'expo-constants';
+import { Button, TextInput, List } from 'react-native-paper';
+
 
 export default function App() {
+
+  const [tasks, setTasks] = useState(['first task'])
+  const [inputText, setInputText] = useState('')
+
+  const onAddButtonPress = () => {
+    if (inputText !== '') {
+      setTasks([...tasks, inputText]);
+      setInputText('');
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <SafeAreaView>
+    <View style={styles.container}> 
+      <Text>This is my fantastic app</Text>
+      <TextInput 
+        label='type a task'
+        onChangeText={text => setInputText(text)}
+        value={inputText}
+      />
+      <Button icon="send" mode="contained" onPress={onAddButtonPress}>
+        Press me
+      </Button>
+      {tasks.map( (task, index) => <Text key={index}>{task}</Text>)}
     </View>
-  );
+    // </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+    flex: 1, 
+    // alignItems: 'center', 
+    // justifyContent: 'center',
   },
-});
+  inputStyle: {
+    height: 40,
+    borderColor: 'green',
+    borderWidth: 1
+  }
+
+})
