3eefa38a644d9fa09eadd42ac317858f4ef28104
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-09-29 06:55:58 -0500

    allow user to scroll through notes

--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
-import {KeyboardAvoidingView} from 'react-native';
-import {List} from 'react-native-paper';
+import React, { useState } from 'react'
+import { ScrollView } from 'react-native';
+import { List } from 'react-native-paper';
 
 import Note from './Note';
 import Task from './Task';
@@ -9,7 +9,7 @@ import AddNoteModal from './AddNoteModal';
 
 const shortid = require('shortid')
 
-function Page({ page, notes, onPressTaskRadioButton, saveNote, deleteNote}) {
+function Page({ page, notes, onPressTaskRadioButton, saveNote, deleteNote }) {
 
   //controls for dialog box
   const [isDialogVisible, setDialog] = useState(false);
@@ -18,52 +18,53 @@ function Page({ page, notes, onPressTaskRadioButton, saveNote, deleteNote}) {
 
   //sets type of dialog box to render, then opens it
   const [noteType, setNoteType] = useState('note');
-  const addNote = () => { setNoteType('note'); showDialog()}
-  const addTask = () => { setNoteType('task'); showDialog()}
+  const addNote = () => { setNoteType('note'); showDialog() }
+  const addTask = () => { setNoteType('task'); showDialog() }
 
   return (
     <>
-      <List.Section>
-
-        <List.Subheader>{page.title}</List.Subheader>
+      <ScrollView>
+        <List.Section>
+          <List.Subheader>{page.title}</List.Subheader>
           {/* <List> */}
-        {page.notes.map( (noteID, index) => {
-          let note = notes[noteID]
-          
-          if (note.type === 'note') {
-            return <Note 
-              note={note}
-              key={shortid.generate()}
-              noteID={noteID}
-              deleteNote={deleteNote}
+          {page.notes.map((noteID, index) => {
+            let note = notes[noteID]
+
+            if (note.type === 'note') {
+              return <Note
+                note={note}
+                key={shortid.generate()}
+                noteID={noteID}
+                deleteNote={deleteNote}
               />
-            } 
-            
+            }
+
             else if (note.type === 'task') {
-              return <Task 
+              return <Task
                 note={note}
                 onPressTaskRadioButton={onPressTaskRadioButton}
                 noteID={noteID}
                 key={shortid.generate()}
                 deleteNote={deleteNote}
-                />
-              }
-               
-            })}
-            {/* </List> */}
-      </List.Section>
-      <AddNoteFAB 
-        addNote={addNote} 
+              />
+            }
+
+          })}
+          {/* </List> */}
+        </List.Section>
+      </ScrollView>
+      <AddNoteFAB
+        addNote={addNote}
         addTask={addTask}
       />
-        <AddNoteModal 
-          isDialogVisible={isDialogVisible} 
-          hideDialog={hideDialog}
-          noteType={noteType}
-          saveNote={saveNote}
-        />
+      <AddNoteModal
+        isDialogVisible={isDialogVisible}
+        hideDialog={hideDialog}
+        noteType={noteType}
+        saveNote={saveNote}
+      />
     </>
-      
+
 
   )
 }
