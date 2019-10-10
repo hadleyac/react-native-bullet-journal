01ff39abe74af1871f4aa000f5055a4f2bb6b5ff
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-10-10 07:11:50 -0500

    Refactor to PureComponents

--- components/Note.js
+++ components/Note.js
@@ -1,19 +1,38 @@
-import React from 'react'
-import { List, Button, IconButton } from 'react-native-paper';
-
-function Note({ note, noteID, deleteNote }) {
-  const { content, important, inspiration } = note;
-  return (
-    <List.Item
-      title={content}
-      description="this is a description"
-      // titleStyle={{ flexWrap: 'wrap', flex: 1 }}
-      left={() => <IconButton size={12} icon="remove" disabled />}
-      right={() => <IconButton icon="delete" size={16} onPress={() => { deleteNote(noteID) }} />}
-    />
+import React, { PureComponent } from 'react'
+import { List, IconButton } from 'react-native-paper';
 
 
-  )
+class Note extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <List.Item
+        title={this.props.content}
+        description="this is a description"
+        // titleStyle={{ flexWrap: 'wrap', flex: 1 }}
+        left={() => <IconButton size={12} icon="remove" disabled />}
+        right={() => <IconButton icon="delete" size={16} onPress={() => { this.props.deleteNote(this.props.noteID) }} />}
+      />
+    )
+  }
+
 }
+// function Note({ note, noteID, deleteNote }) {
+//   const { content, important, inspiration } = note;
+//   return (
+//     <List.Item
+//       title={content}
+//       description="this is a description"
+//       // titleStyle={{ flexWrap: 'wrap', flex: 1 }}
+//       left={() => <IconButton size={12} icon="remove" disabled />}
+//       right={() => <IconButton icon="delete" size={16} onPress={() => { deleteNote(noteID) }} />}
+//     />
+
+
+//   )
+// }
 
 export default Note
--- components/Task.js
+++ components/Task.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import {
   List,
-  Button,
-  Checkbox,
-  RadioButton,
   IconButton
 } from 'react-native-paper';
 import {
   StyleSheet,
-  Text
 } from 'react-native';
 
 
-function Task({ noteID, note, onPressTaskRadioButton, deleteNote }) {
-  const { complete, content, important, inspiration } = note;
-  return (
-    <List.Item
-      title={content}
-      titleStyle={complete ? styles.complete : styles.none}
-      left={() => <IconButton size={12} icon="lens" disabled />}
-      right={() => <IconButton size={16} icon="delete" onPress={() => { deleteNote(noteID) }} />}
-    />
-  )
+class Task extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <List.Item
+        title={this.props.content}
+        titleStyle={this.props.complete ? styles.complete : styles.none}
+        left={() => <IconButton size={12} icon="lens" disabled />}
+        right={() => <IconButton size={16} icon="delete" onPress={() => { this.props.deleteNote(this.props.noteID) }} />}
+      />
+    )
+  }
+
 }
+// function Task({ noteID, note, onPressTaskRadioButton, deleteNote }) {
+//   const { complete, content, important, inspiration } = note;
+//   return (
+//     <List.Item
+//       title={content}
+//       titleStyle={complete ? styles.complete : styles.none}
+//       left={() => <IconButton size={12} icon="lens" disabled />}
+//       right={() => <IconButton size={16} icon="delete" onPress={() => { deleteNote(noteID) }} />}
+//     />
+//   )
+// }
 
 export default Task
 
