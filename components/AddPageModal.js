9732084f9cb279f5a06ba382a663acaba00b3134
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-10-14 17:46:29 -0500

    Allow user to save pages

--- components/AddPageModal.js
+++ components/AddPageModal.js
@@ -5,10 +5,43 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 
 function AddPageModal({ isAddPageModalOpen, toggleAddPageModal, savePage }) {
+  let textInputRef = useRef(null)
   //initalize a timestamp on render
   const [timeStamp, setTimeStamp] = useState(new moment());
   //initialize a page title that defaults to the stringified, formatted, timestamp. 
   const [title, setTitle] = useState(timeStamp.format('ddd d/M/YY'));
+
+  const onPressAddPageButton = () => {
+
+    //hide keyboard
+    textInputRef.current.blur();
+    //hide dialog
+    toggleAddPageModal();
+
+    InteractionManager.runAfterInteractions(() => {
+      if (title) {
+        const page = {
+          date: timeStamp,
+          title: title,
+          key: timeStamp.format('dhms'),
+          notes: [],
+        }
+        savePage(page);
+      }
+
+    });
+    console.log('trying to add a page');
+
+  }
+
+  useEffect(() => {
+    //Will focus the textInput box if the modal is visible
+    if (isAddPageModalOpen) {
+      setTimeout(() => {
+        textInputRef.current.focus()
+      }, 50);
+    }
+  }, [isAddPageModalOpen])
 
   return (
     <Portal>
@@ -21,7 +54,7 @@ function AddPageModal({ isAddPageModalOpen, toggleAddPageModal, savePage }) {
         <Dialog.Title>Add Page</Dialog.Title>
         <Dialog.Content avoidKeyboard>
           <TextInput
-            // ref={textInputRef}
+            ref={textInputRef}
             label='Title'
             mode='outlined'
             value={title}
@@ -30,7 +63,7 @@ function AddPageModal({ isAddPageModalOpen, toggleAddPageModal, savePage }) {
         </Dialog.Content>
         <Dialog.Actions>
           <Button
-            onPress={() => console.log('need save page function')}
+            onPress={onPressAddPageButton}
           >
             Done
             </Button>
