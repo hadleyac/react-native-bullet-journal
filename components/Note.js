05f50b0ddb847b1b0367e3970cbc74e7147cb247
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-10-15 11:31:01 -0500

    Display custom body text in Note

--- components/Note.js
+++ components/Note.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import { List, IconButton } from 'react-native-paper';
+import { Text } from 'react-native';
 
 
 class Note extends PureComponent {
@@ -11,8 +12,9 @@ class Note extends PureComponent {
     return (
       <List.Item
         title={this.props.content}
-        description="this is a description"
-        // titleStyle={{ flexWrap: 'wrap', flex: 1 }}
+        // description={this.props.bodyText}
+        description={() => <Text style={{ color: 'rgba(0,0,0,.54)' }}>{this.props.bodyText}</Text>}
+        titleStyle={{ flexWrap: 'wrap', flex: 1 }}
         left={() => <IconButton size={12} icon="remove" disabled />}
         right={() => <IconButton icon="delete" size={16} onPress={() => { this.props.deleteNote(this.props.noteID) }} />}
       />
