bb37febb530cb2dffb09702b2a559daa089b2c7b
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-11-04 16:58:33 -0600

    Change entry point to Index.js

--- Index.js
+++ Index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import App from './App';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { registerRootComponent } from 'expo';
+import { activateKeepAwake } from 'expo-keep-awake';
 
 //Redux Imports
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducer from './store/reducer'
+
+//This was present in the default AppEntry.js, so I copied it here. 
+if (__DEV__) {
+  activateKeepAwake();
+}
+console.log('are we getting to the index')
 const store = createStore(reducer)
 
 const theme = {
@@ -27,4 +35,4 @@ function Index() {
   )
 }
 
-export default Index;
+export default registerRootComponent(Index);
