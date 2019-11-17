df99ee821e4ae2bd993424b2615714c35c0b0b24
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-11-17 16:06:27 -0600

    Refactor to functional component with useState hooks

--- components/auth/LoginScreen.js
+++ components/auth/LoginScreen.js
@@ -1,101 +1,91 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, SafeAreaView, KeyboardAvoidingView, Alert } from 'react-native'
+import React, { Component, useState } from 'react'
+import { StyleSheet, View, SafeAreaView, KeyboardAvoidingView, Alert, TextInputMa } from 'react-native'
 import { DefaultTheme, Text, TextInput, Button } from 'react-native-paper';
 import * as firebase from 'firebase'
 
-export default class LoginScreen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
+function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  onLoginPress = () => {
-    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+  const onLoginPress = () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
       .then(() => {
-        console.log(this.state.email)
-        console.log(this.state.password)
         console.log('success')
 
       }, (error) => {
-        console.log(this.state.email)
-        console.log(this.state.password)
         console.log('fail')
         Alert.alert(error.message)
       })
   }
-  render() {
-    return (
-      <>
-        <SafeAreaView style={styles.statusBar} />
-        <SafeAreaView style={styles.container} >
-          {/* <KeyboardAvoidingView > */}
-          <Text>
-            Let's Get Going!
-          </Text>
-          <TextInput
-            style={styles.input}
-            label='email'
-            value={this.state.username}
-            onChangeText={email => this.setState({ email })}
-            theme={loginTheme}
-            mode='outlined'
-            dense='true'
-          />
-          <TextInput
-            style={styles.input}
-            label='password'
-            value={this.state.username}
-            onChangeText={password => this.setState({ password })}
-            theme={loginTheme}
-            mode='outlined'
-            dense='true'
-            underlineColor='white'
-          />
-          <Button
-            style={styles.input}
-            theme={loginTheme}
-            color='rgba(255,64,129 ,1)'
-            mode='contained'
-          >
-            signup
-            </Button>
-          <TextInput
-            style={styles.input}
-            label='email'
-            value={this.state.username}
-            onChangeText={email => this.setState({ email })}
-            theme={loginTheme}
-            mode='outlined'
-            dense='true'
-          />
-          <TextInput
-            style={styles.input}
-            label='password'
-            value={this.state.username}
-            onChangeText={password => this.setState({ password })}
-            theme={loginTheme}
-            mode='outlined'
-            dense='true'
-            underlineColor='white'
-          />
-          <Button
-            style={styles.input}
-            theme={loginTheme}
-            color='rgba(255,64,129 ,1)'
-            mode='contained'
-            onPress={this.onLoginPress}
-          >
-            Login
-            </Button>
-          {/* </KeyboardAvoidingView> */}
-        </SafeAreaView>
-      </>
-
-    )
-  }
+  return (
+    <>
+      <SafeAreaView style={styles.statusBar} />
+      <SafeAreaView style={styles.container} >
+        <Text style={{ color: 'white', fontSize: 24, marginBottom: 20 }}>
+          Let's Get Going!
+      </Text>
+        <TextInput
+          style={styles.input}
+          label='email'
+          onChangeText={text => setEmail(text)}
+          theme={loginTheme}
+          mode='outlined'
+          dense={true}
+          autoCapitalize='none'
+        />
+        <TextInput
+          style={styles.input}
+          label='password'
+          onChangeText={text => setPassword(text)}
+          theme={loginTheme}
+          mode='outlined'
+          dense='true'
+          underlineColor='white'
+          secureTextEntry={true}
+        />
+        <Button
+          style={styles.input}
+          theme={loginTheme}
+          color='rgba(255,64,129 ,1)'
+          mode='contained'
+        >
+          signup
+        </Button>
+        <TextInput
+          style={styles.input}
+          label='email'
+          onChangeText={text => setEmail(text)}
+          theme={loginTheme}
+          mode='outlined'
+          dense='true'
+          autoCapitalize='none'
+        />
+        <TextInput
+          style={styles.input}
+          label='password'
+          onChangeText={text => setPassword(text)}
+          theme={loginTheme}
+          mode='outlined'
+          dense='true'
+          underlineColor='white'
+          secureTextEntry={true}
+        />
+        <Button
+          style={styles.input}
+          theme={loginTheme}
+          color='rgba(255,64,129 ,1)'
+          textColor='white'
+          mode='contained'
+          onPress={onLoginPress}
+        >
+          Login
+      </Button>
+        <Text style={{ marginTop: 20, color: 'white', fontSize: 16 }}>
+          Create an account
+      </Text>
+      </SafeAreaView>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -111,7 +101,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#f50057'
   },
   input: {
-    width: '80%'
+    width: '80%',
+    color: 'white'
   },
   button: {
 
@@ -133,3 +124,5 @@ const loginTheme = {
     // accent: '#f50057',
   }
 };
+
+export default LoginScreen
