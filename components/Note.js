f0c0b13b8c2710546c9b27ceb825f46602f985cf
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-11-17 21:02:35 -0600

    Change icon for delete, clean up code formatting

--- components/Note.js
+++ components/Note.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react'
 import { List, IconButton } from 'react-native-paper';
 import { Text } from 'react-native';
 
-
 class Note extends PureComponent {
   constructor(props) {
     super(props)
@@ -12,16 +11,25 @@ class Note extends PureComponent {
     return (
       <List.Item
         title={this.props.content}
-        // description={this.props.bodyText}
-        description={() => <Text style={{ color: 'rgba(0,0,0,.54)' }}>{this.props.bodyText}</Text>}
+        description={() => (
+          <Text
+            style={{ color: 'rgba(0,0,0,.54)' }}>
+            {this.props.bodyText}
+          </Text>)}
         titleStyle={{ flexWrap: 'wrap', flex: 1 }}
-        left={() => <IconButton size={12} icon="minus" disabled />}
-        right={() => <IconButton icon="delete" size={16} onPress={() => { this.props.deleteNote(this.props.noteID) }} />}
+        left={() => (
+          <IconButton
+            size={12}
+            icon="minus"
+            disabled />)}
+        right={() => (
+          <IconButton
+            icon="close"
+            size={16}
+            onPress={() => { this.props.deleteNote(this.props.noteID) }} />)}
       />
     )
   }
-
 }
-
 
 export default Note
--- components/Task.js
+++ components/Task.js
@@ -1,12 +1,6 @@
 import React, { PureComponent } from 'react'
-import {
-  List,
-  IconButton
-} from 'react-native-paper';
-import {
-  StyleSheet,
-} from 'react-native';
-
+import { List, IconButton } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
 
 class Task extends PureComponent {
   constructor(props) {
@@ -18,26 +12,30 @@ class Task extends PureComponent {
       <List.Item
         title={this.props.content}
         titleStyle={this.props.complete ? styles.complete : styles.none}
-        left={() => <IconButton size={12} icon="checkbox-blank-circle" style={styles.icon} disabled />}
-        right={() => <IconButton size={16} icon="delete" onPress={() => { this.props.deleteNote(this.props.noteID) }} />}
+        left={() => (
+          <IconButton
+            size={12}
+            icon="checkbox-blank-circle"
+            style={styles.icon}
+            disabled />)}
+        right={() => (
+          <IconButton
+            size={16}
+            icon="close"
+            onPress={() => { this.props.deleteNote(this.props.noteID) }} />)}
       />
     )
   }
-
 }
+
 export default Task
 
 const styles = StyleSheet.create({
   complete: {
     color: 'grey',
     textDecorationLine: 'line-through',
-    // flexDirection: 'row'
   },
   icon: {
-    // flex: 1,
-    // display: 'flex',
-    // justifyContent: 'center',
-    // alignItems: 'center',
     paddingTop: 5
   }
 })
